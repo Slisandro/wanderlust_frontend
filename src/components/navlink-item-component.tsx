623b4af052ac7d3188e781f5ca16d03996c4b11d
Lisandro <salvareschilisandro0@gmail.com>
2023-10-04 18:15:12 -0300

    dispatch and action in nav links

--- src/components/navlink-item-component.tsx
+++ src/components/navlink-item-component.tsx
@@ -1,11 +1,26 @@
+'use client'
+import { Menu, useMenu } from "@/store";
+
 export default function NavLinkItemComponent({ title, selected }: { title: string, selected: boolean }) {
+    const { type, setType }: Menu = useMenu<Menu>(s => s as Menu);
+
+    const handleSelect = () => setType(title);
+
     return (
-        <li className={
-            selected ?
-                "font-bold cursor-pointer pb-2 px-2 border-b-[6px] border-b-primary text-[20px] font-[600] uppercase text-primary" :
-                "font-bold cursor-pointer pb-2 px-2 border-b-[6px] border-b-[#B0B0D9] text-[20px] font-[600] uppercase text-[#B0B0D9]"}
+        <li
+            className={
+                type === title ?
+                    "font-bold cursor-pointer pb-2 px-2 border-b-[6px] border-b-primary text-[20px] font-[600] uppercase text-primary" :
+                    "font-bold cursor-pointer pb-2 px-2 border-b-[6px] border-b-[#B0B0D9] text-[20px] font-[600] uppercase text-[#B0B0D9]"
+            }
         >
-            {title}
+            <button
+                id={title}
+                className="w-full bg-transparent border-none"
+                onClick={handleSelect}
+            >
+                {title}
+            </button>
         </li>
     )
 }
