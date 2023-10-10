86e7fdb2152230ce4a91bc205fb7ef469e7b3bd4
Lisandro <salvareschilisandro0@gmail.com>
2023-10-09 21:27:59 -0300

    update core components/layout/pages

--- src/core/components/menu-modules-component.tsx
+++ src/core/components/menu-modules-component.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Menu, useMenu } from "@/core/redux";
+import SubmenuAccommodationLayout from "@/modules/alojamiento/layout/submenu-accommodation-layout";
 import AirplaneChipComponent from "@/modules/vuelos/components/airplane-chip-component";
 import SubMenuFlightComponent from "@/modules/vuelos/components/submenu-flights-component";
 
@@ -13,7 +14,9 @@ export default function MenuModulesComponent() {
         </div>
     );
 
-    if (type === "Alojamiento") return <p className="mx-auto">Alojamiento</p>;
+    if (type === "Alojamiento") return <SubmenuAccommodationLayout />;
+
+
     if (type === "Transporte") return <p className="mx-auto">Transporte</p>;
     if (type === "Turismo") return <p className="mx-auto">Turismo</p>;
 
