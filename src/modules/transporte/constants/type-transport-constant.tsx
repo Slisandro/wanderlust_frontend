1e61ead1844789cee9a012884337a49ef87ceeb5
Lisandro <salvareschilisandro0@gmail.com>
2023-10-10 13:56:17 -0300

    constants and layout

---
+++ src/modules/transporte/constants/type-transport-constant.tsx
@@ -0,0 +1,19 @@
+const TypeTransport = [
+    {
+        title: 'Automovil',
+        urlImage: `bg-transport-automovil`,
+        description: "Coche estándar con la posibilidad de trasladar a 4 personas más el conductor"
+    },
+    {
+        title: 'Autobus',
+        urlImage: `bg-transport-autobus`,
+        description: "Transporta numerosas personas y su capacidad, generalmente, varía entre 10 y 120 pasajeros"
+    },
+    {
+        title: 'Microbus',
+        urlImage: `bg-transport-microbus`,
+        description: "Tiene capacidad para transportar hasta 9 personas (8 plazas para pasajeros + la plaza del conductor)"
+    },
+]
+
+export default TypeTransport;
